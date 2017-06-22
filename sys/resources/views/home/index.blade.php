6d37fed8340db672107210eceb071ed5aac74bca
Kaishiyoku <dmp@nfze.de>
2017-06-22 21:47:12 +0200

    added home view

---
+++ sys/resources/views/home/index.blade.php
@@ -0,0 +1,78 @@
+@extends('app')
+
+@section('content')
+    <h2>Projects</h2>
+
+    <div class="card-deck p-t-25 p-b-30">
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Carbon Notes</h4>
+                <p>Simple note taking with many features.</p>
+                <a href="https://www.carbon-notes.de" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">1. Sternenflotten Division</h4>
+                <p>Online Star Trek roleplay community.</p>
+                <a href="https://www.sternenflotten-division.net" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="card-deck p-b-50">
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Network Gaming Clan</h4>
+                <p>Clansite.</p>
+                <a href="https://www.network-gaming-clan.de" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Asphyxiated Dreams</h4>
+                <p>Page of my former band.</p>
+                <a href="https://www.asphyxiated-dreams.de" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Last Escape</h4>
+                <p>Former browsergame project.</p>
+                <a href="https://www.last-escape.net" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+    </div>
+
+
+    <h2>Hosted</h2>
+
+    <div class="card-deck p-t-25">
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Wallabag</h4>
+                <p>Read it later app.</p>
+                <a href="https://wallabag.andreas-wiedel.de" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Fresh RSS</h4>
+                <p>RSS aggregator.</p>
+                <a href="https://rss.andreas-wiedel.de" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="card-block">
+                <h4 class="card-title">Monica</h4>
+                <p>Personal CRM app.</p>
+                <a href="https://monica.andreas-wiedel.de" class="btn btn-primary">Visit site</a>
+            </div>
+        </div>
+    </div>
+@endsection
