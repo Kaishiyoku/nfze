cb7db9121d6d28178d2f2dec72377720d2105f12
Kaishiyoku <dmp@nfze.de>
2020-05-20 19:25:20 +0200

    removed old code for re-development

--- resources/views/shared/server_status.blade.php
+++
@@ -1,5 +0,0 @@
-@if ($isRunning)
-    <i class="fa fa-circle text-success" aria-hidden="true"></i>
-@else
-    <i class="fa fa-circle text-danger" aria-hidden="true"></i>
-@endif
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => ['menus']], function () {
-    Route::get('/', 'HomeController@index')->name('home.index');
-    Route::get('/about_me', 'HomeController@about_me')->name('home.about_me');
-});
